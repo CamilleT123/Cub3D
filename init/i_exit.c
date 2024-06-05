9bb229e64337fe6cc17ca8b7a06ef65695bcedd7
aduvilla <aduvilla@student.42.fr>
2024-06-05 12:26:26 +0200

    error

--- init/i_exit.c
+++ init/i_exit.c
@@ -6,7 +6,7 @@
 /*   By: aduvilla <aduvilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 21:47:01 by aduvilla          #+#    #+#             */
-/*   Updated: 2024/06/04 22:51:49 by aduvilla         ###   ########.fr       */
+/*   Updated: 2024/06/05 12:21:04 by aduvilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 int	map_error(char *s1, char *s2, int status)
 {
-	ft_putstr_fd("cub3d: map: ", 2);
+	ft_putstr_fd("Error\n", 2);
 	ft_putstr_fd(s1, 2);
 	ft_putstr_fd(s2, 2);
 	ft_putstr_fd("\n", 2);
